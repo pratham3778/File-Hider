404d35cfbd8a8123ef82bfba366b0a9a789db485
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2024-10-31 22:08:22 +0530

    Create Welcome.java

---
+++ src/main/java/views/Welcome.java
@@ -0,0 +1,36 @@
+package views;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Welcome {
+    public void welcomeScreen() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to the Application");
+        System.out.println("Press 1 to Login");
+        System.out.println("Press 2 to Sign-up");
+        System.out.println("Press 0 to Exit");
+
+        int choice = 0;
+        try {
+            choice = Integer.parseInt(br.readLine());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        switch (choice) {
+            case 1 -> login();
+            case 2 -> signUp();
+            case 0 -> System.exit(0);
+        }
+    }
+
+    private void signUp() {
+
+    }
+
+    private void login() {
+
+    }
+}
