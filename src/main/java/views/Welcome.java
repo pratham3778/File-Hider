744d2f536d59d3957eb2542f85f76b72d5e243d6
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2024-11-02 22:25:15 +0530

    Update Welcome.java

--- src/main/java/views/Welcome.java
+++ src/main/java/views/Welcome.java
@@ -1,8 +1,16 @@
 package views;
 
+import dao.UserDAO;
+import model.User;
+import service.GenerateOTP;
+import service.SendOTPService;
+import service.UserService;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Welcome {
     public void welcomeScreen() {
@@ -26,11 +34,51 @@ public class Welcome {
         }
     }
 
-    private void signUp() {
-
-    }
 
     private void login() {
+        Scanner sc = new Scanner(System.in);
+        String email = sc.nextLine();
+        try {
+            if (UserDAO.isExists(email)) {
+                String genOTP = GenerateOTP.getOTP();
+                SendOTPService.sendOTP(email, genOTP);
+                System.out.println("Enter the OTP: ");
+                String otp = sc.nextLine();
+                if (otp.equals(genOTP)){
+                    System.out.println("Welcome");
+                } else {
+                    System.out.println("Wrong OTP");
+                }
+            } else {
+                System.out.println("User Not Found ");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void signUp() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter E-mail: ");
+        String email = sc.nextLine();
+
+        String genOTP = GenerateOTP.getOTP();
+        SendOTPService.sendOTP(email, genOTP);
+        System.out.println("Enter the OTP: ");
+        String otp = sc.nextLine();
+        if (otp.equals(genOTP)){
+            User user = new User(name, email);
+            int response = UserService.saveUser(user);
+
+
+            
+            //you left here
+        } else {
+            System.out.println("Wrong OTP");
+        }
 
     }
 }
+
